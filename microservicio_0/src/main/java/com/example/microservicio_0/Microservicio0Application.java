53bb2e2308e08a651180dc3f9d660a3968c5e044
daemunozr <dae.munozr@duocuc.cl>
2026-04-25 13:30:27 -0400

    añadido primer microservicio

---
+++ microservicio_0/src/main/java/com/example/microservicio_0/Microservicio0Application.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservicio0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microservicio0Application.class, args);
+	}
+
+}
---
+++ microservicio_0/src/test/java/com/example/microservicio_0/Microservicio0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicio_0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservicio0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
