b87742c75ddb554691ffa9b197e642a909f24a58
daemunozr <dae.munozr@duocuc.cl>
2026-05-02 18:28:49 -0400

    añadido service de comuna y region. RegionService con errores de compilacion

--- ms_sucursales/src/main/java/com/example/ms_sucursales/service/ComunaService.java
+++ ms_sucursales/src/main/java/com/example/ms_sucursales/service/ComunaService.java
@@ -1,5 +1,39 @@
 package com.example.ms_sucursales.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ms_sucursales.model.Comuna;
+import com.example.ms_sucursales.repository.ComunaRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class ComunaService {
         
+        @Autowired
+        private ComunaRepository repository;
+
+        public List<Comuna> findAll() {
+                return repository.findAll();
+        }
+
+        public Comuna findById(Integer id) {
+                return repository.findById(id);
+        }
+
+        public Comuna findByNombre(String nombre) {
+                return repository.findByNombre(nombre);
+        }
+
+        public Comuna save(Comuna comuna) {
+                return repository.save(comuna);
+        }
+
+        public void deleteById(Long id) {
+                repository.deleteById(id);
+        }
 }
--- ms_sucursales/src/main/java/com/example/ms_sucursales/service/RegionService.java
+++ ms_sucursales/src/main/java/com/example/ms_sucursales/service/RegionService.java
@@ -1,5 +1,39 @@
 package com.example.ms_sucursales.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ms_sucursales.model.Region;
+import com.example.ms_sucursales.repository.RegionRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class RegionService {
-        
+
+        @Autowired
+        private RegionRepository repository;
+
+        public List<Region> findAll() {
+                return repository.findAll();
+        }
+
+        public Region findById(Integer id) {
+                return repository.findById(id);
+        }
+
+        public Region findByNombre(String nombre) {
+                return repository.findByNombre(nombre);
+        }
+
+        public Region save(Region region) {
+                return repository.save(region);
+        }
+
+        public void deleteById(Long id) {
+                repository.deleteById(id);
+        }
 }
